32afe0a76e11addac8f1ad773ff11683efbe6c69
dreamiland <maple_jista@hotmail.com>
2017-09-19 19:48:10 -0500

    Add project files.

---
+++ Actividad6/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actividad6
+{
+    class Repetitions
+    {
+        private string word;
+        private int repetitions;
+        private HashSet<string> repeatsPerFile = new HashSet<string>();
+
+        public Repetitions(string w, string fileName)
+        {
+            word = w;
+            repetitions = 1;
+            repeatsPerFile.Add(fileName);
+        }
+
+        public void addWord(string fileName)
+        {
+            repetitions++;
+            repeatsPerFile.Add(fileName);
+        }
+
+        public override bool Equals(object rc)
+        {
+            return Equals(rc as Repetitions);
+        }
+
+        public string printScore()
+        {
+            return word + "  ;  " + repetitions + " repeticiones  ;  " + repeatsPerFile.Count + " archivos";
+        }
+    }
+}
---
+++ Actividad6/Program.cs
@@ -0,0 +1,87 @@
+﻿using FileOutputs;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actividad6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo d = new DirectoryInfo(Outputs.getAllFiles());
+            FileInfo[] Files = d.GetFiles("*.txt");
+
+            string output_path6 = @"C:\Users\maple\Documents\9° Semester\CS13309_Archivos_HTML\a6_matricula.txt";
+            string output;
+
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            List<string> sortedWords = new List<string>();
+            Dictionary <String, Repetitions> repetitions = new Dictionary<string, Repetitions>();
+            
+            foreach (FileInfo file in Files)
+            {
+                output = "";
+                var watchEach = System.Diagnostics.Stopwatch.StartNew();
+                string htmlContent = File.ReadAllText(file.FullName);
+                htmlContent.Trim();
+                
+                string[] eachWord = htmlContent.Split(' ');
+                
+                try
+                {
+                    foreach (string word in eachWord)
+                    {
+                        if (!string.IsNullOrEmpty(word))
+                        {
+                            if (!word.Equals(" "))
+                            {
+                                word.Replace(",", "")
+                                    .Replace(".", "");
+
+                                if (repetitions.ContainsKey(word))
+                                {
+                                    repetitions[word].addWord(file.Name);
+                                }
+                                else
+                                {
+                                    repetitions.Add(word, new Repetitions(word, file.Name));
+                                }
+                            }
+                        }
+                    }
+
+                }
+                catch (ArgumentNullException argExc)
+                {
+                    Console.WriteLine(argExc.StackTrace);
+                }
+                catch (KeyNotFoundException keyNotFoundExc)
+                {
+                    Console.WriteLine(keyNotFoundExc.StackTrace);
+                }
+
+                foreach (KeyValuePair<string, Repetitions> kvp in repetitions)
+                {
+                    output += "\n" + kvp.Value.printScore() + "\n";
+                    Console.WriteLine(kvp.Value.printScore());
+                }
+                
+                output += "\n" + file.Name + " finished in " + watchEach.Elapsed.TotalMilliseconds.ToString() + " ms";
+                Console.WriteLine(output);
+                watchEach.Stop();
+                Outputs.output_print(output_path6, output);
+            }
+
+            output = "\nAll files sorted in\t" + watch.Elapsed.TotalMilliseconds.ToString() + " ms";
+            Console.WriteLine(output);
+            watch.Stop();
+            Outputs.output_print(output_path6, output);
+
+            Console.Read();
+        }
+    }
+}
